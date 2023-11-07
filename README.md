699850d335be3b00412357eff1353ca0a76dc6b8
James Collings <james@jclabs.co.uk>
2023-11-07 22:39:48 +0000

    Add build script

---
+++ README.md
@@ -0,0 +1,5 @@
+# ImportWP Carbon Fields Importer
+
+Version: __STABLE_TAG__
+Requires Import WP: 2.5.0
+Requires Import WP PRO: 2.5.0
--- carbon-fields.php
+++ carbon-fields.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://www.importwp.com
  * Description: Allow Import WP to import Carbon Fields.
  * Author: James Collings <james@jclabs.co.uk>
- * Version: 0.0.1
+ * Version: __STABLE_TAG__
  * Author URI: https://www.importwp.com
  * Network: True
  */
